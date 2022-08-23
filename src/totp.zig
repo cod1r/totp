0723d2f02a3dfee0b28409cc0a1a736dd95d9ab4
cod1r <jasonho353@gmail.com>
2022-08-23 01:47:32 -0500

    corrected divide amount by seconds not 30 milliseconds

--- src/totp.zig
+++ src/totp.zig
@@ -45,36 +45,40 @@ fn HMAC_SHA3_256(key: []u8, text: []u8) ![]u8 {
     return last_hash;
 }
 
-fn genHOTPVal(key: []u8, text: []u8) !u64 {
+fn genHOTPVal(key: []u8, text: []u8) ![]u8 {
     var sha_value = try HMAC_SHA3_256(key, text);
     var fourbytesidx = sha_value[31] & 0x0F;
-    var bin_code = 
-        (@intCast(u64, sha_value[fourbytesidx] & 0x7F) << 24) | 
-        (@intCast(u64, sha_value[fourbytesidx + 1] & 0xFF) << 16) | 
-        (@intCast(u64, sha_value[fourbytesidx + 2] & 0xFF) << 8) | 
+    var bin_code =
+        (@intCast(u64, sha_value[fourbytesidx] & 0x7F) << 24) |
+        (@intCast(u64, sha_value[fourbytesidx + 1] & 0xFF) << 16) |
+        (@intCast(u64, sha_value[fourbytesidx + 2] & 0xFF) << 8) |
         (@intCast(u64, sha_value[fourbytesidx + 3] & 0xFF));
-    return bin_code % std.math.pow(u64, 10, 6);
+    var digits = bin_code % std.math.pow(u64, 10, 6);
+    var digit_arr: [6]u8 = undefined;
+    for (digit_arr) |_, idx| {
+        digit_arr[5 - idx] = @intCast(u8, (digits % 10) + 48);
+        digits /= 10;
+    }
+    return digit_arr[0..];
 }
 
 pub fn main() !void {
     const time_step_seconds: u64 = 30;
     const T0: u64 = 0;
     var T = std.time.milliTimestamp();
-    
+
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var alloc = gpa.allocator();
 
     var bytes = std.ArrayList(u8).init(alloc);
+    var steps: u64 = @intCast(u64, @divFloor((T - T0), time_step_seconds * 1000));
     var i: u6 = 0;
     while (i <= 7) : (i += 1) {
-        var steps: u64 = @intCast(u64, @divFloor((T - T0), time_step_seconds));
-        try bytes.append(
-            @intCast(u8, 
-                (steps & (@intCast(u64, 255) << (8 * i))) >> (8 * i)));
+        try bytes.append(@intCast(u8, (steps & (@intCast(u64, 255) << (8 * i))) >> (8 * i)));
     }
     var key: []const u8 = "jjj";
     var key_arr = std.ArrayList(u8).init(alloc);
     try key_arr.appendSlice(key);
     var out = try genHOTPVal(key_arr.items, bytes.items);
-    std.debug.print("{}\n", .{out});
+    std.debug.print("{s}\n", .{out});
 }
